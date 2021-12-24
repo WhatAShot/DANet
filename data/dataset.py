67d08eab2fc04fb77713d5c45f3a053e229ba6f3
SteveLiao <32828303+kkk55596@users.noreply.github.com>
2021-12-24 14:51:28 +0800

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -13,20 +13,21 @@ def remove_unused_column(data):
     data.drop(columns=unused_list, inplace=True)
     return data
 
-def split_data(data, target):
+def split_data(data, target, test_size):
     label = data[target]
     data = data.drop([target], axis=1)
-    X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.2, random_state=123, shuffle=True)
+    X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=test_size, random_state=123, shuffle=True)
     return X_train, y_train.values, X_test, y_test.values
 
 
-def quantile_transform(X_train, X_test):
+def quantile_transform(X_train, X_valid, X_test):
     quantile_train = np.copy(X_train)
-    qt = QuantileTransformer(random_state=1330, output_distribution='normal').fit(quantile_train)
+    qt = QuantileTransformer(random_state=55688, output_distribution='normal').fit(quantile_train)
     X_train = qt.transform(X_train)
+    X_valid = qt.transform(X_valid)
     X_test = qt.transform(X_test)
 
-    return X_train, X_test
+    return X_train, X_valid, X_test
 
 def forest_cover():
     target = "Covertype"
@@ -54,36 +55,43 @@ def forest_cover():
     data = pd.read_csv('./data/forest_cover_type/forest-cover-type.csv', header=None, names=feature)
     train_idx = pd.read_csv('./data/forest_cover_type/train_idx.csv', header=None)[0].values
     train = data.iloc[train_idx, :]
+    valid_idx = pd.read_csv('./data/forest_cover_type/valid_idx.csv', header=None)[0].values
+    valid = data.iloc[valid_idx, :]
     test_idx = pd.read_csv('./data/forest_cover_type/test_idx.csv', header=None)[0].values
     test = data.iloc[test_idx, :]
 
 
     y_train = train[target].values
     X_train = train.drop([target], axis=1).values
+    y_valid = valid[target].values
+    X_valid = valid.drop([target], axis=1).values
     y_test = test[target].values
     X_test = test.drop([target], axis=1).values
 
     mean = np.mean(X_train, axis=0)
     std = np.std(X_train, axis=0)
     X_train = (X_train - mean) / std
+    X_valid = (X_valid - mean) / std
     X_test = (X_test - mean) / std
 
-    return X_train, y_train, X_test, y_test
+    return X_train, y_train, X_valid, y_valid, X_test, y_test
 
 def MSLR():
     target = 0
     train = pd.read_pickle('./data/MSLR-WEB10K/train.pkl')
     valid = pd.read_pickle('./data/MSLR-WEB10K/valid.pkl')
     test = pd.read_pickle('./data/MSLR-WEB10K/test.pkl')
-    train = pd.concat([train, valid], axis=0)
 
     y_train = train[target].values
+    y_valid = valid[target].values
     y_test = test[target].values
-    train.drop([target], axis=1, inplace=True)
-    test.drop([target], axis=1, inplace=True)
-    X_train, X_test = quantile_transform(train, test)
 
-    return X_train, y_train, X_test, y_test
+    train.drop([target], axis=1, inplace=True)
+    valid.drop([target], axis=1, inplace=True)
+    test.drop([target], axis=1, inplace=True)
+    X_train, X_valid, X_test = quantile_transform(train, valid, test)
+
+    return X_train, y_train, X_valid, y_valid, X_test, y_test
 
 def yahoo():
     target = 0
@@ -93,78 +101,112 @@ def yahoo():
     train = remove_unused_column(train)
     valid = remove_unused_column(valid)
     test = remove_unused_column(test)
-    train = pd.concat([train, valid], axis=0)
 
     y_train = train[target].values
+    y_valid = valid[target].values
     y_test = test[target].values
+
     train.drop([target], axis=1, inplace=True)
+    valid.drop([target], axis=1, inplace=True)
     test.drop([target], axis=1, inplace=True)
-    X_train, X_test = quantile_transform(train, test)
-    return X_train, y_train, X_test, y_test
+    X_train, X_valid, X_test = quantile_transform(train, valid, test)
+    return X_train, y_train, X_valid, y_valid, X_test, y_test
 
 def yearpred():
     target = 0
-    data = pd.read_pickle('./data/YearPrediction.pkl')
-    label = data[target]
-    data = data.drop([target], axis=1)
-    y_test = label[-51630:].copy().values
-    X_test = data[-51630:].copy()
-    y_train = label[:-51630].copy().values
-    X_train = data[:-51630].copy()
+    data = pd.read_pickle('./data/yearpred/YearPrediction.pkl')
 
-    X_train, X_test = quantile_transform(X_train, X_test)
-    return X_train, y_train, X_test, y_test
+    train_idx = pd.read_csv('./data/yearpred/train_idx.csv')['0'].values
+    train = data.iloc[train_idx, :]
+    valid_idx = pd.read_csv('./data/yearpred/valid_idx.csv')['0'].values
+    valid = data.iloc[valid_idx, :]
+    test_idx = pd.read_csv('./data/yearpred/test_idx.csv')['0'].values
+    test = data.iloc[test_idx, :]
+
+    y_train = train[target].values
+    X_train = train.drop([target], axis=1).values
+    y_valid = valid[target].values
+    X_valid = valid.drop([target], axis=1).values
+    y_test = test[target].values
+    X_test = test.drop([target], axis=1).values
+
+    X_train, X_valid, X_test = quantile_transform(X_train, X_valid, X_test)
+    return X_train, y_train, X_valid, y_valid, X_test, y_test
 
 def epsilon():
     target = 0
     train = pd.read_pickle('./data/epsilon/train.pkl')
     test = pd.read_pickle('./data/epsilon/test.pkl')
 
+    train_idx = pd.read_csv('./data/epsilon/train_idx.csv')['0'].values
+    train = train.iloc[train_idx, :]
+    valid_idx = pd.read_csv('./data/epsilon/valid_idx.csv')['0'].values
+    valid = train.iloc[valid_idx, :]
+
     y_train = train[target].values
+    y_valid = valid[target].values
     y_test = test[target].values
     X_train = train.drop([target], axis=1).values
+    X_valid = valid.drop([target], axis=1).values
     X_test = test.drop([target], axis=1).values
+
 
     mean = np.mean(X_train, axis=0)
     std = np.std(X_train, axis=0)
     X_train = (X_train - mean) / std
+    X_valid = (X_valid - mean) / std
     X_test = (X_test - mean) / std
 
-    return X_train, y_train, X_test, y_test
+    return X_train, y_train, X_valid, y_valid, X_test, y_test
 
 def click():
     target = 'target'
-    data = pd.read_pickle('./data/click.pkl')
-    label = data[target]
-    data.drop([target], axis=1, inplace=True)
-    X_train, X_test = data[:-100_000].copy(), data[-100_000:].copy()
-    y_train, y_test = label[:-100_000].copy().values, label[-100_000:].copy().values
+    data = pd.read_pickle('./data/click/click.pkl')
+
+    train_idx = pd.read_csv('./data/click/train_idx.csv')['0'].values
+    train = data.iloc[train_idx, :]
+    valid_idx = pd.read_csv('./data/click/valid_idx.csv')['0'].values
+    valid = data.iloc[valid_idx, :]
+    test_idx = pd.read_csv('./data/click/test_idx.csv')['0'].values
+    test = data.iloc[test_idx, :]
+
+    y_train = train[target].values
+    X_train = train.drop([target], axis=1)
+    y_valid = valid[target].values
+    X_valid = valid.drop([target], axis=1)
+    y_test = test[target].values
+    X_test = test.drop([target], axis=1)
     cat_features = ['url_hash', 'ad_id', 'advertiser_id', 'query_id',
                     'keyword_id', 'title_id', 'description_id', 'user_id']
 
     cat_encoder = LeaveOneOutEncoder()
     cat_encoder.fit(X_train[cat_features], y_train)
     X_train[cat_features] = cat_encoder.transform(X_train[cat_features])
+    X_valid[cat_features] = cat_encoder.transform(X_valid[cat_features])
     X_test[cat_features] = cat_encoder.transform(X_test[cat_features])
 
-    X_train, X_test = quantile_transform(X_train.astype(np.float32), X_test.astype(np.float32))
-    return X_train, y_train, X_test, y_test
+    X_train, X_valid, X_test = quantile_transform(X_train.astype(np.float32), X_valid.astype(np.float32), X_test.astype(np.float32))
+    return X_train, y_train, X_valid, y_valid, X_test, y_test
 
 def cardio():
     target = 'cardio'
     data = pd.read_csv('./data/cardio/cardiovascular-disease.csv', delimiter=';').drop(['id'], axis=1)
     train_idx = pd.read_csv('./data/cardio/train_idx.csv')['0'].values
     train = data.iloc[train_idx, :]
+    valid_idx = pd.read_csv('./data/cardio/valid_idx.csv')['0'].values
+    valid = data.iloc[valid_idx, :]
     test_idx = pd.read_csv('./data/cardio/test_idx.csv')['0'].values
     test = data.iloc[test_idx, :]
 
     y_train = train[target].values
     train.drop([target], axis=1, inplace=True)
+    y_valid = valid[target].values
+    valid.drop([target], axis=1, inplace=True)
     y_test = test[target].values
     test.drop([target], axis=1, inplace=True)
 
-    X_train, X_test = quantile_transform(train, test)
-    return X_train, y_train, X_test, y_test
+    X_train, X_valid, X_test = quantile_transform(train, valid, test)
+    return X_train, y_train, X_valid, y_valid, X_test, y_test
 
 
 def get_data(datasetname):
